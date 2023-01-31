2978f658010611c8e69a6da5f30e25a9f80422b0
DaisukeKawamura <da1i9su9ke7.0715@icloud.com>
2023-01-31 23:51:04 +0900

    prepare home screen

---
+++ src/screen/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  flex: 1;
+  background: orange;
+`;
+
+const HomeScreen = () => {
+  return <Container></Container>;
+};
+
+export default HomeScreen;
